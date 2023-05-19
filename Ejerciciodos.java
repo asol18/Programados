cfc10852e1fefcdf30b27c8f69a8c939a40cd155
Allyson Sequeira <allysequi18@gmail.com>
2023-05-18 20:32:29 -0600

    Archivo txt uno

--- Ejerciciodos.java
+++ Ejerciciodos.java
@@ -5,6 +5,6 @@ public class EjercicioDos{
         System.out.println("Ingrese un valor en celsius");
         int celsius = input.nextInt();
         double fahrenheit =(9.0/5)*celsius + 32;
-        System.out.println("El resultado es:" + fahrenheit)
+        System.out.println("El resultado es:" + fahrenheit);
     }
 }
