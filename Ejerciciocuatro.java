d90375a0b8f4387416b3074cd22e2d951b20e65e
Allyson Sequeira <allysequi18@gmail.com>
2023-05-20 19:04:33 -0600

    Archivo txt

--- Ejerciciocuatro.java
+++ Ejerciciocuatro.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
-public class EjercicioCuatro{
-    public static void main (String[] args){
-        Scanner scanner= new Scanner(System.in);
+
+public class EjercicioCuatro {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese un radio");
-        int radio = nextInt();
-    }
-    func area (int x){
-        double pi =(3,14);
-        radio=x * x;
-        double area = pi*radio;
-        System.out.println("El área da" + area);
-        return area
+        int radio = scanner.nextInt();
+        
+        EjercicioCuatro ejercicio = new EjercicioCuatro();
+        ejercicio.area(radio);
+        ejercicio.perimetro(radio);
     }
 
-    func perimetro(y int){
-        double pi =3,14
-        double perimetro =2 *pi *y;
-        System.out.println("El perímetro es" + perimetro);
-        return perimetro
+    public void area(int x) {
+        double pi = 3.1416;
+        double radio = x * x;
+        double area = pi * radio;
+        System.out.println("El área es de: " + area);
+    }
+
+    public void perimetro(int y) {
+        double pi = 3.1416;
+        double perimetro = 2 * pi * y;
+        System.out.println("El perímetro es de: " + perimetro);
     }
 }
---
+++ Ejerciciotres.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EjercicioTres{
+    public static void main(String[] args){
+        int numero_1;
+        int numero_2;
+        int resultado;
+
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Ingrese el primer número: ");
+        numero_1= scanner.nextInt();
+        System.out.println("Ingrese el segundo número: ");
+        numero_2= scanner.nextInt();
+        resultado= numero_1+numero_2
+        System.out.println("El resultado es: " +resultado)
+    }
+}
+
+
