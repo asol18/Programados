f99be9936ed2513441157b11e114bf57746cd065
Allyson Sequeira <allysequi18@gmail.com>
2023-05-21 23:18:56 -0600

    Archivo txt

--- Ejerciciocinco.java
+++ Ejerciciocinco.java
@@ -1,16 +1,29 @@
+
 import java.util.Scanner;
 
 public class EjercicioCinco{
-    public static void main(String[] args);
+    public static void main(String[] args){
     Scanner scanner= new Scanner(System.in);
-    System.out.println("Ingrese un número: ");
-    int num= scanner nextInt();
-    System.out.println("El factorial de :" + num + "es" + fac);
+
+    System.out.print("Ingrese un número: ");
+    int numero= scanner.nextInt();
+    scanner.close();
+
+    double factorial= calcularFactorial(numero);
+    System.out.println("El factorial de: " + numero + " es " + factorial);
+    } 
+    public static double calcularFactorial(int n){
+      if (n == 0 || n == 1){
+        return 1;
+      }else {
+        double factorial= 1;
+        for (int i= 2; i<= n; i++){
+          factorial*= i;
+        }
+        return factorial;
+      }
+  } 
 }
-public static double factorial(int n){
-    double aux= 1;
-    for (int i=2 i<= n; i++){
-        aux*= i;
-    }
-    return aux;
-}
+
+
+ 
---
+++ Ejercicioseis.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class EjercicioSeis{
+    public static void main(Strings[] args){
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Ingrese un número entero:");
+        for(int i= 1; i<= 10; i++){
+            int resultado= numero * i;
+            System.out.println(numero + "x" +i+ "=" + resultado);
+        }
+    }
+}
