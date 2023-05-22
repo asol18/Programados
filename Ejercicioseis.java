e2ffeaf9e8f430610428ad2af231d320b9faf1e1
asol18 <133692452+asol18@users.noreply.github.com>
2023-05-21 23:29:41 -0600

    Update Ejercicioseis.java

--- Ejercicioseis.java
+++ Ejercicioseis.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 public class EjercicioSeis{
-    public static void main(Strings[] args){
+    public static void main(String[] args){
         Scanner scanner= new Scanner(System.in);
         System.out.print("Ingrese un número entero:");
         for(int i= 1; i<= 10; i++){
+            int numero= scanner.nextInt();
             int resultado= numero * i;
             System.out.println(numero + "x" +i+ "=" + resultado);
         }
